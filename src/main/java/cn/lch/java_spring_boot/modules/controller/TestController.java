2dd192edfdec41e4d0f9a77c266feb2b28846563
liuchenhu <1346942720@qq.com>
2020-08-10 19:45:24 +0800

    "2020.8.10 作业 "

--- src/main/java/cn/lch/java_spring_boot/modules/controller/TestController.java
+++ src/main/java/cn/lch/java_spring_boot/modules/controller/TestController.java
@@ -1,15 +1,58 @@
 package cn.lch.java_spring_boot.modules.controller;
 
-import org.springframework.stereotype.Controller;
+import cn.lch.java_spring_boot.modules.vo.ApplicatonTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
+    @Value("${com.port}")
+    private Integer port;
+    @Value("${com.name}")
+    private String name;
+    @Value("${com.age}")
+    private Integer age;
+    @Value("${com.random}")
+    private String desc;
+
     /**
-     *@ http://localhost:8080/hello
+     * http://localhost:8081/logTest
+     */
+    @GetMapping("/logTest")
+    @ResponseBody
+    public String logTest() {
+        LOGGER.trace("This is trace log");
+        LOGGER.debug("This is debug log");
+        LOGGER.info("This is info log");
+        LOGGER.warn("This is warn log");
+        LOGGER.error("This is error log");
+        return "This is log test";
+    }
+    /**
+     * http://localhost:8081/config
+     */
+    @Autowired
+    ApplicatonTest test;
+    @GetMapping("config")
+    @ResponseBody
+    public String config(){
+        String str =
+                new StringBuffer().append(port).append("------").append(name)
+                        .append("-------").append(age).append("-------").append(desc).append("<br>")
+                .append(test.getPort()).append("-------").append(test.getName())
+                        .append("---------").append(test.getAge()).append("--------").append(test.getDesc()).toString();
+        return str;
+    }
+    /**
+     *@ http://localhost:8081/hello
      */
 
     @GetMapping("hello")
---
+++ src/main/java/cn/lch/java_spring_boot/modules/vo/ApplicatonTest.java
@@ -0,0 +1,47 @@
+package cn.lch.java_spring_boot.modules.vo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:config/applicationTest.properties")
+@ConfigurationProperties(prefix = "com")
+public class ApplicatonTest {
+    private Integer port;
+    private String name;
+    private Integer age;
+    private String desc;
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
