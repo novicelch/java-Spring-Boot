8f6c76c40093e34de2de090981d9794c0de5c734
liuchenhu <1346942720@qq.com>
2020-08-12 20:21:01 +0800

    "2020.8.12 作业 "

--- src/main/java/cn/lch/java_spring_boot/modules/controller/CityController.java
+++ src/main/java/cn/lch/java_spring_boot/modules/controller/CityController.java
@@ -1,6 +1,7 @@
 package cn.lch.java_spring_boot.modules.controller;
 
 import cn.lch.java_spring_boot.modules.Service.CityService;
+import cn.lch.java_spring_boot.modules.common.vo.Result;
 import cn.lch.java_spring_boot.modules.common.vo.SearchVo;
 import cn.lch.java_spring_boot.modules.entity.City;
 import com.github.pagehelper.PageInfo;
@@ -14,7 +15,7 @@ public class CityController {
     @Autowired
     private CityService cityService;
     /**
-     *  http://127.0.0.1/cities/522   GET
+     *  http://localhost/cities/522   GET
      */
     @GetMapping("/cities/{countryId}")
     public List<City> getCityByCountryId(@PathVariable int countryId){
@@ -22,11 +23,47 @@ public class CityController {
     }
 
     /**
-     *  http://127.0.0.1/cities/522   POST
+     *  http://localhost/cities/522   POST
      *  {"currentPage":"1","pageSize":"5"}
      */
     @PostMapping(value = "/cities/{countryId}",consumes = "application/json")
     public PageInfo<City> getCitiesBySearchVo(@PathVariable int countryId, @RequestBody SearchVo searchVo){
         return cityService.getCitiesBySearchVo(countryId,searchVo);
+    }
+
+
+    /**
+     * http://localhost/cities --- post
+     * {"currentPage":"1","pageSize":"5","keyWord":"Sh","orderBy":"population","sort":"desc"}
+     */
+    @PostMapping(value = "/cities", consumes = "application/json")
+    public PageInfo<City> getCitiesBySearchVo(@RequestBody SearchVo searchVo) {
+        return cityService.getCitiesBySearchVo(searchVo);
+    }
+
+    /**
+     * http://localhost/city ---- post
+     * {"cityName":"test1","localCityName":"freeCity","countryId":"522"}
+     */
+    @PostMapping(value = "/city", consumes = "application/json")
+    public Result<City> insertCity(@RequestBody City city) {
+        return cityService.insertCity(city);
+    }
+
+    /**
+     * http://localhost/city ---- put
+     * "cityId"="2259",cityName"="MingGuang"
+     */
+    @PutMapping(value = "/city", consumes = "application/x-www-form-urlencoded")
+    public Result<City> updateCity(@ModelAttribute City city) {
+        return cityService.updateCity(city);
+    }
+
+    /**
+     * http://localhost/city/2258 ---- delete
+     */
+    @DeleteMapping("/city/{cityId}")
+    public Result<Object> deleteCity(@PathVariable int cityId) {
+        return cityService.deleteCity(cityId);
     }
 }
