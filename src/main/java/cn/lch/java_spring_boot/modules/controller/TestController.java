99e30cf646a39985822a5cddf7f5b41e28e0bd2c
liuchenhu <1346942720@qq.com>
2020-08-18 16:12:36 +0800

    "2020.8.18 作业 "

--- src/main/java/cn/lch/java_spring_boot/modules/controller/TestController.java
+++ src/main/java/cn/lch/java_spring_boot/modules/controller/TestController.java
@@ -9,15 +9,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -126,4 +133,86 @@ public class TestController {
         String paramValue2 = request.getParameter("paramKey");
         return "This is test module desc." + paramValue + "==" + paramValue2;
     }
+
+    /**
+     *  单个文件上传  localhost/test/index ---- post
+     */
+    @PostMapping(value = "/file", consumes = "multipart/form-data")
+    public String uploadFile(@RequestParam MultipartFile file, RedirectAttributes redirectAttributes) {
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select file.");
+            return "redirect:index";
+        }
+        try {
+            String destFilePath = "C:\\Users\\liuchenhu\\Desktop\\upload\\" + file.getOriginalFilename();
+            File destFile = new File(destFilePath);
+            file.transferTo(destFile);
+
+            redirectAttributes.addFlashAttribute("message", "Upload file success.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            redirectAttributes.addFlashAttribute("message", "Upload file failed.");
+        }
+        return "redirect:/test/index";
+    }
+
+    /**
+     *  多个文件上传 localhost/test/index ---- post
+     */
+    @PostMapping(value = "/files",consumes = "multipart/form-data")
+    public String uploadFiles(@RequestParam MultipartFile[] files,
+                              RedirectAttributes redirectAttributes) {
+        boolean empty = true;
+        try {
+            for (MultipartFile file : files) {
+                if (file.isEmpty()) {
+                    continue;
+                }
+
+                String destFilePath = "C:\\Users\\liuchenhu\\Desktop\\upload\\" + file.getOriginalFilename();
+                File destFile = new File(destFilePath);
+                file.transferTo(destFile);
+                empty = false;
+            }
+
+            if (empty) {
+                redirectAttributes.addFlashAttribute(
+                        "message", "Please select file.");
+            } else {
+                redirectAttributes.addFlashAttribute(
+                        "message", "Upload file success.");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            redirectAttributes.addFlashAttribute(
+                    "message", "Upload file failed.");
+        }
+
+        return "redirect:/test/index";
+    }
+
+    /**
+     * 文件下载 localhost/test/index ---- get
+     */
+    @GetMapping("/file")
+    public ResponseEntity<Resource> downloadFile(@RequestParam String fileName) {
+        Resource resource = null;
+        try {
+            resource = new UrlResource(
+                    Paths.get("C:\\Users\\liuchenhu\\Desktop\\upload\\" + fileName).toUri());
+            if (resource.exists() && resource.isReadable()) {
+                return ResponseEntity
+                        .ok()
+                        .header(HttpHeaders.CONTENT_TYPE, "application/octet-stream")
+                        .header(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment; filename=\"%s\"", resource.getFilename()))
+                        .body(resource);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
 }
